7358bdcf730f1b37136c226311a3c93cb713fa8a
edureka <abhardwaj273@gmail.com>
2023-04-15 23:04:48 +0530

    first commit

---
+++ file1.java
@@ -0,0 +1,19 @@
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld");
+		System.out.println("Welcome to Edureka");
+		System.out.println("Welcome to Continous testing batch");
+		System.out.println("Welcome to Data Science");
+		System.out.println("Welcome to IOT");
+		System.out.println("Welcome to cloud computing");
+		System.out.println("Welcome to Big data");
+		System.out.println("Welcome to Azure");
+		System.out.println("Welcome to GCP");
+		System.out.println("Good Morning");
+		
+
+	}
+
+}
---
+++ file2.java
@@ -0,0 +1,19 @@
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld1");
+		System.out.println("Welcome to Edureka1");
+		System.out.println("Welcome to Continous testing batch1");
+		System.out.println("Welcome to Data Science1");
+		System.out.println("Welcome to IOT1");
+		System.out.println("Welcome to cloud computing1");
+		System.out.println("Welcome to Big data1");
+		System.out.println("Welcome to Azure1");
+		System.out.println("Welcome to GCP1");
+		System.out.println("Good Morning1");
+		
+
+	}
+
+}
